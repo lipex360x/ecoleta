988f716f0d4159db35fa49c6683dcb0a09c608b7
Felipe Borges <lipex360@gmail.com>
2021-01-19 19:19:40 -0300

    Page CreatePoint init

---
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+const CreatePoint = () => {
+  return (
+    <>
+      <S.Header>Hello CreatePoint</S.Header>
+    </>
+  )
+}
+
+export default CreatePoint
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import CreatePoint from '../pages/CreatePoint'
 
 const Routes = () => (
   <Switch>
     <Route path="/" component={Home} exact />
+    <Route path="/create-point" component={CreatePoint} />
   </Switch>
 )
 
